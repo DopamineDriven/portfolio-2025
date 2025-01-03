e7ecdcba77e7b582f1d4952f175668e62d21fee8
Andrew Ross <andrew.ross@takeda.com>
2025-01-02 21:52:24 -0600

    progress

--- apps/web/mdx-components.tsx
+++ apps/web/mdx-components.tsx
@@ -1,11 +1,19 @@
 import type { MDXComponents } from "mdx/types";
 import { CodeBlock } from "./src/components/CodeBlock";
+import Image, { type ImageProps } from 'next/image'
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
     pre: ({ children, ...props }: React.ComponentPropsWithoutRef<"pre">) => {
       return <CodeBlock {...props}>{children}</CodeBlock>;
-    }
+    },
+    img: (props) => (
+      <Image
+        sizes="100vw"
+        style={{ width: '100%', height: 'auto' }}
+        {...(props as ImageProps)}
+      />
+    ),
   };
 }
