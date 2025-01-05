bf6b030a2798079d949a516145e3e378d5a94f2c
Andrew Ross <andrew.ross@takeda.com>
2025-01-05 04:41:59 -0600

    up

--- apps/chess/src/app/layout.tsx
+++ apps/chess/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Toast as Toaster } from "@/ui/toast";
+// import { Toast as Toaster } from "@/ui/toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
-        <Toaster />
+        {/* <Toaster /> */}
       </body>
     </html>
   );
