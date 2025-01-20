5c892ee0cd2ab45b4e4a332c7a68c6aeb1d328a7
Andrew Ross <andrew.ross@takeda.com>
2025-01-20 03:25:49 -0600

    up

--- apps/chess-v2/src/ui/eval-bar/index.tsx
+++ apps/chess-v2/src/ui/eval-bar/index.tsx
@@ -1,8 +1,14 @@
 "use client";
 
+import type { ChessColor } from "@/utils/chess-types";
 import { toChessGroundColorHelper } from "@/lib/color-helper";
 import { cn } from "@/lib/utils";
-import { ChessColor } from "@/utils/chess-types";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from "@/ui/atoms/tooltip";
 import { scoreToPercent } from "@/utils/score-to-percent";
 
 export function EvalBar({
@@ -17,31 +23,40 @@ export function EvalBar({
   const chessGroundColor = toChessGroundColorHelper(playerColor);
   const toPercent = scoreToPercent(evalScore);
   return (
-    <div
-      className={cn(
-        "mb-1 !h-4 !w-[min(80dvh,100dvw)] sm:mb-0 sm:mr-1 sm:!h-[min(80dvh,95dvw)] sm:!w-10",
-        !isMobile && chessGroundColor === "black"
-          ? "rotate-180"
-          : isMobile && chessGroundColor === "white"
-            ? "rotate-180"
-            : ""
-      )}>
-      <div
-        className={cn(
-          "relative h-4 w-full overflow-hidden bg-black sm:h-full sm:w-10"
-        )}>
+    <TooltipProvider>
+      <Tooltip>
         <div
           className={cn(
-            "absolute bottom-0 left-0 h-4 w-full overflow-hidden bg-white transition-transform duration-500 ease-in sm:h-full sm:w-10"
-          )}
-          style={{
-            transform: isMobile
-              ? `translate3d(${toPercent}%,0,0)`
-              : `translate3d(0,${toPercent}%,0)`
-          }}
-        />
-      </div>
-    </div>
+            "mb-1 !h-4 !w-[min(80dvh,100dvw)] sm:mb-0 sm:mr-1 sm:!h-[min(80dvh,95dvw)] sm:!w-10",
+            !isMobile && chessGroundColor === "black"
+              ? "rotate-180"
+              : isMobile && chessGroundColor === "white"
+                ? "rotate-180"
+                : ""
+          )}>
+          <TooltipContent side={isMobile ? "top" : "bottom"}>
+            <p>{`${evalScore}`}</p>
+          </TooltipContent>
+          <TooltipTrigger asChild>
+            <div
+              className={cn(
+                "relative h-4 w-full overflow-hidden bg-black sm:h-full sm:w-10"
+              )}>
+              <div
+                className={cn(
+                  "absolute bottom-0 left-0 h-4 w-full overflow-hidden bg-white transition-transform duration-500 motion-ease-in-out-quad sm:h-full sm:w-10"
+                )}
+                style={{
+                  transform: isMobile
+                    ? `translate3d(${toPercent}%,0,0)`
+                    : `translate3d(0,${toPercent}%,0)`
+                }}
+              />
+            </div>
+          </TooltipTrigger>
+        </div>
+      </Tooltip>
+    </TooltipProvider>
   );
 }
 
