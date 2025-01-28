899c5dbcc538e4586502198886b7a4102da34780
Andrew Ross <andrew.ross@takeda.com>
2025-01-28 15:18:43 -0600

    fixd deprecation warnings around rehype-pretty-codes usage of getHighlighter in shiki v2+ -- made a patch

--- apps/chess-v2/src/ui/atoms/slider.tsx
+++ apps/chess-v2/src/ui/atoms/slider.tsx
@@ -22,32 +22,29 @@ const Slider: FC<SliderProps> = ({
   <SliderPrimitive.Root
     ref={ref}
     className={cn(
-      "relative flex w-full touch-none select-none items-center",
+      "relative flex w-full touch-none items-center select-none",
       className
     )}
     {...props}>
     <SliderPrimitive.Track
       className={cn(
         "relative h-1.5 w-full grow overflow-hidden rounded-full",
-        trackClassName ? trackClassName : "bg-primary/20"
+        trackClassName ?? "bg-primary/20"
       )}>
       <SliderPrimitive.Range
-        className={cn(
-          "absolute h-full",
-          rangeClassName ? rangeClassName : "bg-primary"
-        )}
+        className={cn("absolute h-full", rangeClassName ?? "bg-primary")}
       />
     </SliderPrimitive.Track>
     <SliderPrimitive.Thumb
       className={cn(
         "block h-4 w-4 rounded-full border shadow-sm transition-colors focus-visible:outline-hidden disabled:pointer-events-none disabled:opacity-50",
-        thumbClassName
-          ? thumbClassName
-          : "border-primary/50 bg-background focus-visible:ring-1 focus-visible:ring-ring"
+        thumbClassName ??
+          "border-primary/50 bg-background focus-visible:ring-ring focus-visible:ring-1"
       )}
     />
   </SliderPrimitive.Root>
 );
+
 Slider.displayName = SliderPrimitive.Root.displayName;
 
 export { Slider };
