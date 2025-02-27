d67281c4efa044097514d031edf90d7d5b065f44
Andrew Ross <andrew.ross@takeda.com>
2025-02-27 03:21:18 -0600

    new resume

--- apps/web/src/utils/__generated__/resume-blob.ts
+++ apps/web/src/utils/__generated__/resume-blob.ts
@@ -13,11 +13,11 @@ export const resumeData = {
 export const filesDataArr = {
   files: [
     {
-      id: "resume-2025",
-      url: "https://ypuktmwmnilhirdf.public.blob.vercel-storage.com/resume-2025-1Owk79GQQY5TqcjLpNrd1lX0fjve5h.pdf",
+      id: "Resume-2025",
+      url: "https://ypuktmwmnilhirdf.public.blob.vercel-storage.com/Resume-2025-zgIy7A5Sk3QOBwCRcfqx6tVVlyiasZ.pdf",
       downloadUrl:
-        "https://ypuktmwmnilhirdf.public.blob.vercel-storage.com/resume-2025-1Owk79GQQY5TqcjLpNrd1lX0fjve5h.pdf?download=1",
-      pathname: "resume-2025.pdf",
+        "https://ypuktmwmnilhirdf.public.blob.vercel-storage.com/Resume-2025-zgIy7A5Sk3QOBwCRcfqx6tVVlyiasZ.pdf?download=1",
+      pathname: "Resume-2025.pdf",
       size: 120793,
       uploadedAt: "2025-02-03T12:26:31.000Z"
     }
