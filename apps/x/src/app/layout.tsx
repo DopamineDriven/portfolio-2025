ae3afcd2cf5ce99a82f56f41e3e54e35f785c37f
Andrew Ross <andrew.ross@takeda.com>
2025-03-31 02:17:13 -0500

    fixed pong

---
+++ apps/x/src/app/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from "next";
+import {
+  BasisGrotesqueProBlack,
+  BasisGrotesqueProBlackItalic,
+  BasisGrotesqueProBold,
+  BasisGrotesqueProBoldItalic,
+  BasisGrotesqueProItalic,
+  BasisGrotesqueProLight,
+  BasisGrotesqueProLightItalic,
+  BasisGrotesqueProMedium,
+  BasisGrotesqueProMediumItalic,
+  BasisGrotesqueProRegular
+} from "@/lib/fonts";
+import { cn } from "@/lib/utils";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "React Pong",
+  description: "React Pong Game"
+};
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={cn(
+        BasisGrotesqueProLight.variable,
+        BasisGrotesqueProMedium.variable,
+        BasisGrotesqueProMediumItalic.variable,
+        BasisGrotesqueProRegular.variable,
+        BasisGrotesqueProItalic.variable,
+        BasisGrotesqueProLightItalic.variable,
+        BasisGrotesqueProBold.variable,
+        BasisGrotesqueProBlack.variable,
+        BasisGrotesqueProBlackItalic.variable,
+        BasisGrotesqueProBoldItalic.variable
+      )}>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ apps/x/src/ui/home/index.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { GentleText } from "@d0paminedriven/motion";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-[100dvh] flex-col items-center justify-center bg-gray-900 p-4">
+      <GentleText
+        content="X Integraton App"
+        textClassName="text-4xl font-bold text-white mb-6"
+        maxWidth="fit"
+        containerClassName="mx-auto"
+        animateOnlyInView={true}
+        autoPlay
+        animationOptions={{ repeatType: "loop" }}
+        as="h1"
+        animateTarget="chars"
+      />
+    </main>
+  );
+}
