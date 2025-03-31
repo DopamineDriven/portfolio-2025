9443a1d9c4b63217ac811f841ee2c07d2dd47217
Andrew Ross <andrew.ross@takeda.com>
2025-03-31 00:25:08 -0500

    fixed pong

--- apps/pong/src/hooks/use-mobile.ts
+++ apps/pong/src/hooks/use-mobile.ts
@@ -3,15 +3,28 @@
 import { useEffect, useState } from "react";
 
 export function useMobile() {
-  const [isMobile, setIsMobile] = useState(false);
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
+  const checkIsMobile = () => {
+    const isTouchDevice =
+      "ontouchstart" in window ||
+      (navigator.maxTouchPoints && navigator.maxTouchPoints > 0) ||
+      window.matchMedia("(pointer: coarse)").matches;
+
+    const vmin = Math.min(window.innerWidth, window.innerHeight);
+
+    setIsMobile(isTouchDevice && vmin < 500);
+  };
+
   useEffect(() => {
-    const checkIfMobile = () => {
-      setIsMobile(window.innerWidth <= 500);
-    };
-    checkIfMobile();
-    window.addEventListener("resize", checkIfMobile);
+    checkIsMobile();
+
+    window.addEventListener("resize", checkIsMobile);
+    window.addEventListener("orientationchange", checkIsMobile);
+
     return () => {
-      window.removeEventListener("resize", checkIfMobile);
+      window.removeEventListener("resize", checkIsMobile);
+      window.removeEventListener("orientationchange", checkIsMobile);
     };
   }, []);
 
