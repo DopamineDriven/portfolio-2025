9e58dfc4c561407f654a6c19d83dbb33096a29b8
Andrew Ross <andrew.ross@takeda.com>
2025-03-31 21:18:56 -0500

    fixed pong

--- apps/x/src/utils/test.ts
+++ apps/x/src/utils/test.ts
@@ -15,31 +15,31 @@ const client = new TwitterApi({
 } satisfies TwitterApiTokens);
 const readonlyClient = client.readOnly;
 
-export const getUserData = async (username: string) => {
-  const user = await readonlyClient.v2.user(username, {
-    "user.fields": "description,public_metrics"
-  });
+export const getUserData = async (userId: string) => {
+  // const user = await readonlyClient.v2.user(username, {
+  //   "user.fields": "description,public_metrics"
+  // });
   // [article,attachments,author_id,card_uri,community_id,context_annotations,conversation_id,created_at,display_text_range,edit_controls,edit_history_tweet_ids,entities,geo,id,in_reply_to_user_id,lang,media_metadata,non_public_metrics,note_tweet,organic_metrics,possibly_sensitive,promoted_metrics,public_metrics,referenced_tweets,reply_settings,scopes,source,text,withheld]'
-  const tweets = await readonlyClient.v2.userTimeline(user.data.id, {
+  const tweets = await readonlyClient.v2.userTimeline(userId, {
     "tweet.fields": "created_at,text,public_metrics",
+    pagination_token: "7140dibdnow9c7btw4azvoo59x4dgbbb352xyfu9gysdx",
     max_results: 100
   });
-  const nextToken = tweets.meta.next_token;
+  const previousToken = tweets.meta.previous_token ?? null;
+  const nextToken = tweets.meta.next_token ?? null;
   const result_count = tweets.meta.result_count;
-  tweets.fetchNext(100);
   return {
-    profile: user.data,
+    // profile: user.data,
     tweets: tweets.data.data,
+    previousToken,
     nextToken,
     result_count,
-    userId: user.data.id
+    userId
   };
 };
 
 (async () => {
-  return await getUserData("Dopamine_Driven");
+  return await getUserData("989610823105568769");
 })().then(d => {
-  console.log(d?.profile ? d.profile : "no profile");
-  console.log(d.tweets.length > 0 ? d.tweets.slice(0, 10) : "no tweets");
-  fs.withWs("src/utils/__out__/x.data.json", JSON.stringify(d, null, 2));
+  fs.withWs(`src/utils/__out__/4.json`, JSON.stringify(d, null, 2));
 });
