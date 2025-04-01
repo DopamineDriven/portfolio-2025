cf7c479e6d2ab5a9154f4a50b8259ba7bcaec43d
Andrew Ross <andrew.ross@takeda.com>
2025-03-31 21:05:36 -0500

    fixed pong

--- apps/pong/src/hooks/use-mobile.ts
+++ apps/pong/src/hooks/use-mobile.ts
@@ -1,19 +1,28 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import Cookies from "cookie.js";
 
 export function useMobile() {
   const [isMobile, setIsMobile] = useState<boolean>(false);
 
   const checkIsMobile = () => {
+    const viewportCookie = Cookies.get("viewport");
+
     const isTouchDevice =
       "ontouchstart" in window ||
       (navigator.maxTouchPoints && navigator.maxTouchPoints > 0) ||
       window.matchMedia("(pointer: coarse)").matches;
 
     const vmin = Math.min(window.innerWidth, window.innerHeight);
-
-    setIsMobile(isTouchDevice && vmin < 500);
+    const deviceIsMobile = isTouchDevice && vmin < 500;
+    if (viewportCookie === "mobile") {
+      setIsMobile(true);
+    } else if (viewportCookie === "desktop") {
+      setIsMobile(false);
+    } else {
+      setIsMobile(deviceIsMobile);
+    }
   };
 
   useEffect(() => {
--- apps/x/src/app/page.tsx
+++ apps/x/src/app/page.tsx
@@ -1,13 +1,12 @@
 import { Suspense } from "react";
-import { getUserData } from "@/lib/x-api";
 import Home from "@/ui/home";
 
-export default async function HomePage() {
-  const data = await getUserData("@Dopamine_Driven");
+export default function HomePage() {
+  // const data = await getUserData("@Dopamine_Driven");
 
   return (
     <Suspense fallback={"Loading..."}>
-      <Home data={data} />
+      <Home data={undefined} />
     </Suspense>
   );
 }
