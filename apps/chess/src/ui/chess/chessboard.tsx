9035cfcc9be342075f57f0f6bd5c9e14d49b1f8a
Andrew Ross <andrew.ross@takeda.com>
2025-09-25 16:18:06 -0500

    updated resume

--- apps/chess/src/ui/chess/chessboard.tsx
+++ apps/chess/src/ui/chess/chessboard.tsx
@@ -25,14 +25,12 @@ const Chessboard: React.FC<ChessboardProps> = ({
     <div
       className={cn(
         `relative grid h-96 w-96 grid-cols-8 gap-0 border-4 border-gray-800`,
-        (className ??= "")
+        className
       )}>
       {board.map((row, rowIndex) =>
         row.map((piece, colIndex) => {
           const isSelected =
-            selectedSquare &&
-            selectedSquare[0] === rowIndex &&
-            selectedSquare[1] === colIndex;
+            selectedSquare?.[0] === rowIndex && selectedSquare[1] === colIndex;
           const isPossibleMove = possibleMoves.some(
             ([r, c]) => r === rowIndex && c === colIndex
           );
@@ -45,11 +43,11 @@ const Chessboard: React.FC<ChessboardProps> = ({
                 isLight ? "bg-amber-200" : "bg-amber-800",
                 isSelected ? "bg-[hsl(60,100%,50%)]/90" : "",
                 isPossibleMove
-                  ? "relative before:absolute before:left-1/2 before:top-1/2 before:h-4 before:w-4 before:-translate-x-1/2 before:-translate-y-1.5 before:rounded-full before:bg-black/15"
+                  ? "relative before:absolute before:top-1/2 before:left-1/2 before:h-4 before:w-4 before:-translate-x-1/2 before:-translate-y-1.5 before:rounded-full before:bg-black/15"
                   : "",
                 `flex h-12 w-12 cursor-pointer items-center justify-center`
               )}
-              onClick={() => onSquareClick && onSquareClick(rowIndex, colIndex)}
+              onClick={() => onSquareClick?.(rowIndex, colIndex)}
               whileHover={{ scale: 1.05 }}
               whileTap={{ scale: 0.95 }}>
               {piece && <ChessPiece piece={piece} />}
